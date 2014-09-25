cfa709ac4156da16ee7fc94bdeeb92d24dcc5431
Justin Huang <jstn@cs.washington.edu>
2014-09-24 22:27:46 -0700

    Clean includes.

--- src/publish_points_node.cpp
+++ src/publish_points_node.cpp
@@ -1,18 +1,10 @@
 // Reads tabletop segmentation clusters and publishes the point cloud.
 
-#include <functional>
 #include <ros/ros.h>
-#include <sensor_msgs/PointCloud.h>
-#include <vector>
 
-#include "rviz_objdetect_caller/Clusters.h"
 #include "rviz_objdetect_caller/publish_points.h"
 
-using rviz_objdetect_caller::Clusters;
 using rviz_objdetect_caller::PointPublisher;
-using sensor_msgs::ChannelFloat32;
-using sensor_msgs::PointCloud;
-using std::vector;
 
 int main(int argc, char** argv) {
   ros::init(argc, argv, "publish_points_node");
